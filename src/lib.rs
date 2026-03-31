faae72c7af2222294b75ddd6285231affcc6f8fa
jmalek <jan.malek@gmail.com>
2026-03-31 01:08:31 +0100

    Revert "experiment: Let's see if nostd was simply in the wrong place"
    This reverts commit 7f2207c8d1c8834ec5afba48cd2510b2ed36ca15.

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,8 @@ If a copy of the MPL was not distributed with this file,
 You can obtain one at https://mozilla.org/MPL/2.0/. 
 */
 
-#![no_std]
 #![doc = include_str!("../README.md")]
+#![no_std]
 
 pub use cranium_core::*;
 
